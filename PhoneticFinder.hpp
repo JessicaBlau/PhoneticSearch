101810d07df08f849e3326b3eb2a4ff0d43a2ce5
JessicaBlau <jblau1993@gmail.com>
2020-03-29 14:09:28 +0300

    Jblau1993@gmail.com

--- PhoneticFinder.hpp
+++ PhoneticFinder.hpp
@@ -3,5 +3,12 @@
 using namespace std;
 
 namespace phonetic{
-    string find(string text, string word) ;
+      string find(string text, string word) ;
+    string chack_Word(string text, string ans,int index);
+    int next_word(string text, int i);
+    bool same_Char(char x , char y);
+
+    int next_Word(string text, int i);
+
+
 }
